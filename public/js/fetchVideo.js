83841979a654f80a9b974e9ebf4ebd1593c4baf4
stevensuki <steven.535210058@stu.untar.ac.id>
2023-06-30 14:53:52 +0700

    output the upload video

--- public/js/fetchVideo.js
+++ public/js/fetchVideo.js
@@ -1,8 +1,24 @@
+const videoArea = document.querySelector('.video-area');
+
 const takeVideo = async () => {
   try {
     const res = await fetch('/getvideo');
     const data = await res.json();
     console.log(data);
+    let userVideo = '';
+    data.videoPosted.forEach((video) => {
+      userVideo += `
+                    <div class="video-container">
+                      <div class="fileVideo style="background-image: url('${video.thumbnail}')"></div>
+                      <div class="video-info">
+                        <h3>${video.title}</h3>
+                        <p>${video.description}</p>
+                        <p>${video.uploadAt}</p>
+                      </div>
+                    </div>
+                  `;
+    });
+    videoArea.innerHTML = userVideo;
   } catch (error) {
     console.log(error);
   }

72f4986e7a3aac79232780286b795f01a2026bea
stevensuki <steven.535210058@stu.untar.ac.id>
2023-07-03 19:26:12 +0700

    update output youtube css

--- public/js/playVideo.js
+++ public/js/playVideo.js
@@ -1,11 +1,28 @@
-videoId = window.location.pathname.split('/').pop();
+const videoId = window.location.pathname.split('/').pop();
 videoDetailPlay(videoId);
+
+const videoPlaying = document.querySelector('.container-video-area');
 
 async function videoDetailPlay(videoId) {
   try {
     const res = await fetch(`/getVideo/${videoId}`);
     const data = await res.json();
-    console.log(data);
+    const dataDetail = data.videoDetail;
+    const videoPlayContainer = document.createElement('div');
+    videoPlayContainer.classList.add('video-play-container');
+    videoPlayContainer.innerHTML = `
+        <div class="fileVideo-play"></div>
+        <div class="video-info-play">
+          <h1>${dataDetail.title}</h1>
+          <br/>
+          <p>
+            ${dataDetail.uploadAt}
+            <br/>
+            ${dataDetail.description}
+          </p>
+        </div>
+      `;
+    videoPlaying.appendChild(videoPlayContainer);
   } catch (err) {
     console.log('No video info');
   }
